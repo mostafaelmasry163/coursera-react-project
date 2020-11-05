0a70032b48da19a103099b982b4ebabd00a8fa3a
mostafaelmasry163 <mostelmasry93@gmail.com>
2020-11-05 05:03:31 +0200

    container components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -51,12 +51,14 @@ class DishDetail extends Component{
             return(<div></div>);
         else
             return(
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderComments(this.props.dish.comments)}
+                        </div>
                     </div>
                 </div>
             );
