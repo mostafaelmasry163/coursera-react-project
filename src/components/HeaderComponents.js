7650d2eb6b4772f1979f29db9822db22f73810d3
mostafaelmasry163 <mostelmasry93@gmail.com>
2020-11-08 03:06:24 +0200

    header and footer

---
+++ src/components/HeaderComponents.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+class Header extends Component {
+    render(){
+        return (
+            <>
+             <Navbar dark>
+                <div className="container">
+                   <NavbarBrand href="/">
+                    Ristorante Con Fusion
+                    </NavbarBrand>
+                </div>
+             </Navbar>
+             <Jumbotron>
+                 <div className="container">
+                     <div className="row row-header">
+                         <div className="col-12 col-sm-6">
+                             <h1>Ristorante Con Fusion</h1>
+                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias deserunt eaque rem. Necessitatibus, veniam debitis.</p>
+                         </div>
+                     </div>
+                 </div>
+             </Jumbotron>
+            </>
+        );
+    }
+}
+
+export default Header;
