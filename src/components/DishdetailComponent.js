0eefed349ecc185e64b87848073146f2a7111f04
mostafaelmasry163 <mostelmasry93@gmail.com>
2020-11-22 04:10:22 +0200

    react animation

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength =(len) => (val) => !(val) || (val.length <= len);
@@ -110,6 +111,8 @@ class DishDetail extends Component{
  renderDish(dish){
         
             return(
+            <FadeTransform in
+                transformProps={{exitTransform: 'scale(0.5) translateY(-50%)'}}>
                 <Card>
                   <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                   <CardBody>
@@ -117,6 +120,7 @@ class DishDetail extends Component{
                       <CardText>{dish.description}</CardText>
                     </CardBody>  
                 </Card>
+            </FadeTransform>
             );
         
     }
@@ -127,16 +131,18 @@ class DishDetail extends Component{
 
         comments= comments.map(com => {
             return(
-                <li key={com.id}>
-                    <p>{com.comment}</p>
-                    <p> -- {com.author}, &nbsp;
-                        {new Intl.DateTimeFormat("en-US", {
-                            year: "numeric",
-                            month: "long",
-                            day: "2-digit"
-                          }).format(new Date(com.date))
-                        } </p>
-                </li>
+                <Fade in>
+                    <li key={com.id}>
+                        <p>{com.comment}</p>
+                        <p> -- {com.author}, &nbsp;
+                            {new Intl.DateTimeFormat("en-US", {
+                                year: "numeric",
+                                month: "long",
+                                day: "2-digit"
+                            }).format(new Date(com.date))
+                            } </p>
+                    </li>
+                </Fade>
             );
         });
 
@@ -144,7 +150,9 @@ class DishDetail extends Component{
             <div>
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
-                    {comments}
+                    <Stagger in>
+                        {comments}
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
